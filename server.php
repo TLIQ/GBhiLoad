f59f911cadb607ea1346e88b282773bd04297da7
TLIQ <12932894+TLIQ@users.noreply.github.com>
2021-01-22 23:37:00 +0300

    Lesson 7

---
+++ server.php
@@ -0,0 +1,16 @@
+<?php
+// server.php
+use Ratchet\Server\IoServer;
+use MyApp\Chat;
+
+require 'vendor/autoload.php';
+require 'chat.php';
+
+$server = IoServer::factory(
+        new Chat(),
+        8181
+);
+
+$server->run();
+
+?>
